a3c521d91dd03d0e65ebcb53c7f4075188786e07
joshypeace <joshuachilapondwa2@gmail.com>
2024-08-01 11:43:05 +0200

    yake ija yatheka

--- Asignup.php
+++ Asignup.php
@@ -4,8 +4,18 @@ require_once "connection.php";
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     $name=$_POST["fname"];
     $surName=$_POST["lname"];
+    $resultFlag=0;
 
-    if($connection && !empty($name) && !empty($surName)){
+    if($connection){
+        $fetch="SELECT * FROM users WHERE fname=$name AND lname=$surName";
+        $result=$connection->query($fetch);
+
+        if($result->num_rows>0){
+            $resultFlag=1;
+        }
+    }
+ 
+    if($connection && !empty($name) && !empty($surName) && $resultFlag){
         $sql="INSERT INTO users (fname,lname) VALUES (?,?)";
         $stmt=$connection->prepare($sql);
         $stmt->bind_param("ss",$name,$surName);
@@ -17,11 +27,13 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 
         $stmt=null;
         $connection=null;
-    
+        
+        echo $name;
+        echo $surName;
+    }else{
+        header("Location:index.php? oops something went wrong");
     }
-    
-    echo $name;
-    echo $surName;
+
 
     
 
