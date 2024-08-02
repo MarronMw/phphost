a0729a1429d9cf1126c14df0ab38cfad8d8f18ea
joshypeace <joshuachilapondwa2@gmail.com>
2024-08-02 10:46:31 +0200

    a new resvisit on tne asignup

--- Asignup.php
+++ Asignup.php
@@ -1,40 +1,30 @@
 <?php
 require_once "connection.php";
 
-if($_SERVER["REQUEST_METHOD"]=="POST" && $connection){
-    $name=$_POST["fname"];
-    $surName=$_POST["lname"];
-  
-        $result=$connection->query("SELECT fname, lname FROM users WHERE users.fname=$name AND users.lname=$surName;");
-        $csql="SELECT * FROM users WHERE users.fname=? AND users.lname=?";
-        $check=$connection->prepare($csql);
-        $check->bind_param("ss",$name,$surName);
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    if ($connection){
+
+        $sql="SELECT * FROM users WHERE users.fname=? AND users.lname=?";
+        $check=$connection->prepare($sql);
+        $check->bind_param("ss",$_POST["fname"],$_POST["lname"]);
         $check->execute();
         $result=$check->get_result();
-
+    
         if($result->num_rows>0){
-            header("Location:index.php? oops something went wrong");
-            $check->close();
-            $connection->close();
-        }else{
-
-            if( !empty($name) && !empty($surName)){
-                $sql="INSERT INTO users (fname,lname) VALUES (?,?)";
-                $stmt=$connection->prepare($sql);
-                $stmt->bind_param("ss",$name,$surName);
-                if($stmt->execute()){
-                    echo "Inserted";
-                }else{
-                    echo "Error inserting into db";
-                }
-        
-                $stmt=null;
-                $connection=null;
-                
-                echo $name;
-                echo $surName;
-            }
+            header("location:index.php? oops looks like the user is already registered");
         }
+    
+        $query="INSERT INTO users (fname,lname) VALUES (?,?)";
+        $stmt=$connection->prepare($query);
+        $stmt->bind_param("ss",$_POST["fname"],$_POST["lname"]);
+        if($stmt->execute()){
+            echo "User sucessfully registered";
+        }else{
+            echo "failed to register user";
+        }
+        $stmt->close();
+        $connection->close();
+    }
 }else{
     echo "Bad Gate Way";
 }
